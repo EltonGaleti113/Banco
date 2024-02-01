5dcc612afca6710fbc23f72c96968b3264cf8697
EltonGaleti113 <egaleti2003@gmail.com>
2024-01-31 22:26:17 -0300

    switch options added

--- src/Programa.java
+++ src/Programa.java
@@ -1,3 +1,22 @@
-public class Programa {
+import java.util.Scanner;
 
+public class Programa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sistema de Banco Coffe Quente");
+        System.out.println("Selecione uma opção. \n 1.Ver Saldo \n 2.Selecionar Conta Corrente");
+
+
+        int option = sc.nextInt();
+
+        switch (option){
+            case 1:
+                System.out.println();
+                break;
+            case 2:
+                System.out.println("");
+                break;
+        }
+    }
 }
